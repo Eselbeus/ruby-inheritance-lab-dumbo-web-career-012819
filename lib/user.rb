61e6a143337c82c751efb495d247e2e945aefb30
eselbeus <eselbeus@gmail.com>
2019-02-02 19:44:26 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,11 @@
 class User 
   
-  attr_accessor :first, :last
+  attr_accessor :first_name, :last_name
   
-  def initialize
-    @first = first 
-    @last = last
+  # def initialize
+  #   @first = first 
+  #   @last = last
     
-  end
+  # end
   
 end
