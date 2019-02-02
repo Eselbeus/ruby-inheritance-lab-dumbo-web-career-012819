8027415b86e9a216ea4091339d5bd729ed9caf40
eselbeus <eselbeus@gmail.com>
2019-02-02 19:50:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,13 @@
 require_relative "./user.rb"
 
-attr_reader :knowledge
-
 class Student < User
   def initialize
     @knowledge = []
   end
   def learn(astr)
     @knowledge << astr
+  end
+  def knowledge
+    @knowledge
   end
 end
